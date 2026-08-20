ef722da299849e2de829656136f6bbcddf71fe32
Jignesh20070119 <192411154.simats@saveetha.com>
2026-08-20 10:53:38 +0530

    Add files via upload

---
+++ Lab ex18/Lab ex18.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("TRAILING(E) = { +, *, ), id }\n");
+    printf("TRAILING(T) = { *, ), id }\n");
+    printf("TRAILING(F) = { ), id }\n");
+    return 0;
+}
+
